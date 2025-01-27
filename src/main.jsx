33c3a0b486a4951fbf24bc91af3d1d7dd1d016b0
Aura Tao <auratao.model@gmail.com>
2025-01-27 08:55:54 +0800

    the first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import App2 from './App2';
+createRoot(document.getElementById('root')).render(
+ 
+    <App2 />
+ 
+)
